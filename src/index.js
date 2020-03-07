2d00769b774e11afa5a79e39d9469d9430ef8b3d
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2020-03-07 17:38:47 +0100

    Add eslint etc.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { SVG } from '@svgdotjs/svg.js'
 
 const mainSVG = SVG('#layer1')
 
-mainSVG.each(function (i, children) {
+mainSVG.each(function(i, children) {
   const color = this.css('fill')
   this.mouseover(() => {
     this.css({ fill: '#f06', stroke: '#000', 'stroke-width': 1, 'stroke-opacity': 1 })
